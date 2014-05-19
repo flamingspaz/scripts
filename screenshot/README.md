0b0374d9474f52d93c54f2ce138192f1fab0611d
Yousef Alam <flamingspaz96@googlemail.com>
2014-05-19 12:02:10 +0100

    screenshot script

---
+++ screenshot/README.md
@@ -0,0 +1,6 @@
+screenshot
+=======
+
+Script to take a screenshot, upload to a web server via sftp and copies the URL to the clipboard.
+
+Requires slop and maim.
---
+++ screenshot/screenshot.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+type=$1
+dur=$2
+if [ "$type" != "image" ]; then
+	if [ "$type" != "video" ]; then
+		type="image"
+	fi
+fi
+
+# Server stuff goes here
+server="yousef@hydrogen.navalrp.co.uk:/srv/www/y96.pw/${type}s"
+temp="/tmp"
+url="http://y96.pw/${type}s"
+imageEncoding=".png"
+videoEncoding=".webm"
+name=`cat /dev/urandom | tr -cd 'a-f0-9' | head -c 8`
+pidFile="/tmp/seen_record.pid"
+
+if [ "$type" == "image" ]; then
+	eval `slop --nokeyboard`
+	if [ "$(($W+$H))" != "0" ]; then
+		maim -g="${W}x${H}+${X}+${Y}" "${temp}/${name}${imageEncoding}"
+		sftp "${server}" <<< "put "${temp}/${name}${imageEncoding}" "${name}${imageEncoding}""
+		if [ $? -eq 0 ]; then
+			notify-send '<b>Screenshot uploaded!</b>' "${url}/${name}${imageEncoding}"
+		else
+			error=$?
+			notify-send "Screenshot upload failed" "Error ${error}"
+		fi
+		echo -n "${url}/${name}${imageEncoding}" | xclip -selection clipboard
+	fi
+else
+	if [ -f "$pidFile" ]; then
+		pid=`cat "$pidFile"`
+		kill $pid
+		sftp "${server}" <<< "put "${temp}/${name}${imageEncoding}" "${name}${imageEncoding}""
+		if [ $? -eq 0 ]; then
+			notify-send 'Video uploaded!'
+		else
+			notify-send 'Video upload failed' 'Error $?'
+		fi
+		echo -e "\a"
+		rm "$pidFile"
+		exit 0
+	fi
+	eval `slop --nokeyboard`
+	if [ "$(($W+$H))" != "0" ]; then
+		echo -e "\a"
+		ffmpeg -f x11grab -s "${W}x${H}" -i ":0.0+${X},${Y}" -an "${temp}/${name}${videoEncoding}" &
+		pid=$!
+		echo -n "$pid" > /tmp/seen_record.pid
+		echo -n "${url}/${name}${videoEncoding}" | xclip -selection clipboard
+		sleep ${dur}
+				pid=`cat "$pidFile"`
+		kill $pid
+		sftp "${server}" <<< "put "${temp}/${name}${videoEncoding}" "${name}${videoEncoding}""
+		if [ $? -eq 0 ]; then
+			notify-send 'Video uploaded!'
+		else
+			notify-send 'Video upload failed: Error $?'
+		fi
+		echo -e "\a"
+		rm "$pidFile"
+		exit 0
+	fi
+fi
